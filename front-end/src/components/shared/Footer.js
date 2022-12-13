a8c8b33069f4bbf5db1d92d1bedacc3466b62035
Nathania Novenrodumetasa <merapic4@gmail.com>
2022-12-13 22:04:14 +0800

    front-end
    belum gabung BE

---
+++ front-end/src/components/shared/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Footer = () => {
+    return (
+        <>
+            <h6>FindYourMusicGenre &copy; 2022</h6>
+        </>
+    );
+};
+
+export default Footer;
---
+++ front-end/src/components/shared/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div class="judul">
+        <h1>Find Your Music Genre</h1>
+        </div>
+    );
+};
+
+export default Header;
